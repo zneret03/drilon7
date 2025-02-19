aae2787ac591cae126b489f33f3e9070f255efd9
Ian Drilon <ian.drilon@mashupgarage.com>
2025-02-19 17:58:46 +0800

    removing apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,10 @@
-import ApolloClient, { InMemoryCache } from "apollo-boost"
-import React, { useEffect, useState, Fragment } from "react"
+import React, { Fragment } from "react"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-import { ApolloProvider } from "react-apollo"
 import Head from "next/head"
 
 function App({ Component, pageProps }) {
-  const [currentServer, setCurrentServer] = useState("")
 
-  const DEVELOPMENT_SERVER = "http://localhost:1337/graphql"
-  const PRODUCTION_SERVER = "https://strapi-drilon7.herokuapp.com/graphql"
-  const LOCALHOST_SERVER = "http://localhost:3000/"
-
-  const SERVER_URL =
-    currentServer === LOCALHOST_SERVER ? DEVELOPMENT_SERVER : PRODUCTION_SERVER
-
-  const client = new ApolloClient({
-    uri: SERVER_URL,
-    cache: new InMemoryCache(),
-  })
-
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-        this.navigator.serviceWorker
-          .register("/sw.js")
-          .then(function (registration: { scope: string }) {
-            console.log(
-              "Service Worker registration successful with scope: ",
-              registration.scope
-            )
-
-            setCurrentServer(registration.scope)
-          }),
-          function (err: any) {
-            console.log("Service Worker registration failed: ", err)
-          }
-      })
-    }
-  }, [])
 
   return (
     <Fragment>
@@ -49,9 +15,7 @@ function App({ Component, pageProps }) {
           <link rel="shortcut icon" href="/image/favicon.ico" />
         </Head>
       </div>
-      <ApolloProvider client={client}>
         <Component {...pageProps} />
-      </ApolloProvider>
     </Fragment>
   )
 }
